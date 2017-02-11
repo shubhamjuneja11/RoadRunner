9d6a1c62ba7cd577fbece15ea4c5b06a8e46d223
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-02-11 12:56:15 +0530

    plo

--- app/src/main/java/com/example/junejaspc/roadrunner/controller/FirebaseListAdapter.java
+++ app/src/main/java/com/example/junejaspc/roadrunner/controller/FirebaseListAdapter.java
@@ -136,7 +136,7 @@ public abstract class FirebaseListAdapter<T> extends BaseAdapter {
 
             @Override
             public void onCancelled(FirebaseError firebaseError) {
-                Log.e("com.example.junejaspc.roadrunner.controller.FirebaseListAdapter", "Listen was cancelled, no more updates will occur");
+                Log.e("FirebaseListAdapter", "Listen was cancelled, no more updates will occur");
             }
 
         });
