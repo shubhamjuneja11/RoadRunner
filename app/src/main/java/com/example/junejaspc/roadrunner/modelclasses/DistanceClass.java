f7172d627de412b6bc51d13eb12a72ea0a17c9bf
shubhamjuneja11 <shubhamjuneja11@gmail.com>
2017-02-11 10:14:53 +0530

    map

---
+++ app/src/main/java/com/example/junejaspc/roadrunner/modelclasses/DistanceClass.java
@@ -0,0 +1,10 @@
+package com.example.junejaspc.roadrunner.modelclasses;
+
+/**
+ * Created by junejaspc on 2/11/2017.
+ */
+
+public class DistanceClass {
+    public String email;
+    public double distance;
+}
